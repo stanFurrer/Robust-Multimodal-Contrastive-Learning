fc1add745c9bc13f7dd7c5065d03069a66dc3f24
Stanislas Furrer <sfurrer@tik43x.ee.ethz.ch>
2021-07-02 16:03:57 +0200

    prepare NLVR2

---
+++ prepare_data.py
@@ -0,0 +1,4 @@
+from vilt.utils.write_nlvr2 import make_arrow
+dataset_images_path = "/itet-stor/sfurrer/net_scratch/nlvr"
+saved_path          = "/itet-stor/sfurrer/net_scratch/nlvr/nlvr2_vilt"
+make_arrow(dataset_images_path, saved_path)
