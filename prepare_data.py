df2b077d640f40deb4a2583ef1a835615ec48279
StanFurrer <stanislas.furrer@epfl.ch>
2021-07-08 16:06:24 +0200

    useless

--- prepare_data.py
+++
@@ -1,4 +0,0 @@
-from vilt.utils.write_nlvr2 import make_arrow
-dataset_images_path = "/itet-stor/sfurrer/net_scratch/nlvr"
-saved_path          = "/itet-stor/sfurrer/net_scratch/nlvr/nlvr2_vilt"
-make_arrow(dataset_images_path, saved_path)
